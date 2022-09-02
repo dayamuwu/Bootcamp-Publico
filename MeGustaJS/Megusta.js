df14aa8ff1c2a8ad352de113a69af5c47e5a7ed3
Tu Nombre <you@example.com>
2022-09-02 01:06:41 -0400

    me gusta

---
+++ MeGustaJS/Megusta.js
@@ -0,0 +1,21 @@
+
+function likeneil(){
+    var var_num_element = document.querySelector("#num1");
+    var var_num_value = document.getElementById("num1").innerText;
+
+    var_num_element.innerHTML =  parseInt(var_num_value)+1;
+}
+
+function likeni(){
+    var var_num_element = document.querySelector("#num2");
+    var var_num_value = document.getElementById("num2").innerText;
+
+    var_num_element.innerHTML =  parseInt(var_num_value)+1;
+}
+
+function likejim(){
+    var var_num_element = document.querySelector("#num3");
+    var var_num_value = document.getElementById("num3").innerText;
+
+    var_num_element.innerHTML =  parseInt(var_num_value)+1;
+}

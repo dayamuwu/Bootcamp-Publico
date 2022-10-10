9a49d459e56b721569c59cc767713670b47772e6
Tu Nombre <you@example.com>
2022-10-10 02:05:25 -0300

    FlaskHTML

---
+++ FlaskTablaHTML/Tabla/controllers/controlador.py
@@ -0,0 +1,19 @@
+from Tabla import app
+from flask import render_template, request
+
+
+@app.route('/')
+def index():
+    usuarios_info = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    return render_template("index.html" , usuarios = usuarios_info)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return "ESTA RUTA NO FUE ENCONTRADA", 404
+    #return render_template('404.html'), 404
---
+++ FlaskTablaHTML/server.py
@@ -0,0 +1,5 @@
+from Tabla import app
+from Tabla.controllers import controladordes
+
+if __name__ =="__main__":
+    app.run(debug=True)

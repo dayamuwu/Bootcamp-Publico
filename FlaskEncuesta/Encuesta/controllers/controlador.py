91239aeb9dac04f236675d970d51cf9690e4942f
Tu Nombre <you@example.com>
2022-10-11 02:15:15 -0300

    Encuesta Dojo

--- FlaskEncuesta/Encuesta/controllers/controlador.py
+++
@@ -1,8 +0,0 @@
-from Encuesta import app
-from flask import render_template, request, redirect, session
-app.secret_key = 'DFG5841'
-
-
-@app.route('/')
-def blabla():
-    return render_template("index.html")
